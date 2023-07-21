c8c0c50eb70988f04e435a3b5c9526e3011da82b
m.venkatapraveen <m.venkatapraveen@mvenkatapraveens-MacBook-Air.local>
2023-07-21 13:02:59 +0530

    Concept :- AJAC Call : XMLHttpRequest

--- starter/script.js
+++ starter/script.js
@@ -4,3 +4,60 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+// Concept :- AJAC Call : XMLHttpRequest
+///////////////////////////////////////
+
+let renderCountry = function (data, className = '') {
+  let html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 10000000
+      ).toFixed(1)}</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+let getCountryData = function (country) {
+  // => AJAX call country
+  let request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  // => Render country
+  request.addEventListener('load', function () {
+    let [data] = JSON.parse(this.responseText);
+    console.log(data);
+    renderCountry(data);
+
+    // => Get neighbour county
+
+    let neighbour = data.borders[0];
+    if (!neighbour) return;
+
+    // => AJAX call country 2
+
+    let request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      let data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+// getCountryData('Republic of India');
+getCountryData('usa');
